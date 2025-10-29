723ccd855287a04a45a50b3e63a496109335dbeb
Yousef eslam <yousefeslam214@gmail.com>
2025-10-29 20:40:54 +0300

    feat(analytics): add Meta Pixel snippet reference and default ID 853924177590110

--- client/src/components/analytics/MetaPixel.tsx
+++ client/src/components/analytics/MetaPixel.tsx
@@ -4,8 +4,32 @@ interface MetaPixelProps {
   id?: string;
 }
 
-// export default function MetaPixel({ id = "1743635966210784" }: MetaPixelProps) {
-export default function MetaPixel({ id = "800700262549089" }: MetaPixelProps) {
+/*
+  Meta Pixel snippet (reference):
+
+  <!-- Meta Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '853924177590110');
+  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=853924177590110&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Meta Pixel Code -->
+
+  The component below implements the same initialization programmatically.
+*/
+
+// Use the requested Pixel ID as the default
+export default function MetaPixel({ id = "853924177590110" }: MetaPixelProps) {
   useEffect(() => {
     // If fbq already exists, don't re-initialize
     if ((window as any).fbq) {
