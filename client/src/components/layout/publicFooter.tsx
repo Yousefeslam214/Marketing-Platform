9f6b88e168fce75793d1b301a9a3b168eac1f0f4
fouadkhalied <foukha49@gmail.com>
2026-06-02 20:13:54 +0300

    fix: new phone number added

--- client/src/components/layout/publicFooter.tsx
+++ client/src/components/layout/publicFooter.tsx
@@ -147,7 +147,7 @@ const PublicFooter = () => {
               </li>
               <li className="flex items-center">
                 <i className="fas fa-phone mx-2"></i>
-                +966 583 898 606
+                583898606
               </li>
 
               <li className="mt-4">
